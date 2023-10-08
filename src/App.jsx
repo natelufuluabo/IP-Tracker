2a5e62c2f7533c548b6896112230925d062b2da1
Nathan Lufuluabo <45732868+natelufuluabo@users.noreply.github.com>
2023-10-08 14:38:57 -0400

    extracted the absolute container in its own file. same for the map container

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import './styles.css';
 import { useState, useEffect } from 'react';
+import AbsoluteContainer from './components/AbsoluteContainer';
+import MapContainer from './components/MapContainer';
 import desktopBg from './images/pattern-bg-desktop.png';
 import mobileBg from './images/pattern-bg-mobile.png';
 
@@ -10,9 +12,7 @@ function App() {
     const handleResize = () => {
       setScreenWidth(window.innerWidth);
     };
-
     window.addEventListener('resize', handleResize);
-
     // Remove the event listener when the component unmounts
     return () => {
       window.removeEventListener('resize', handleResize);
@@ -24,35 +24,8 @@ function App() {
       <div className="bgImage">
         <img src={imgSrc} />
       </div>
-      <div className="mapContainer"></div>
-      <div className="absoluteContainer">
-        <h1>IP Address Tracker</h1>
-        <div className="ipInputCotainer">
-          <input type='text' className='ipInput' placeholder='Search for IP address ou domain' />
-          <button><i className="fa-solid fa-chevron-right"></i></button>
-        </div>
-        <div className='infosContainer'>
-          <div className='infoContainer'>
-            <h3 className='title'>IP ADDRESS</h3>
-            <h3 className='info'>192.212.174.101</h3>
-          </div>
-          <button>|</button>
-          <div className='infoContainer'>
-            <h3 className='title'>LOCATION</h3>
-            <h3 className='info'>Brooklyn, NY 10001</h3>
-          </div>
-          <button>|</button>
-          <div className='infoContainer'>
-            <h3 className='title'>TIMEZONE</h3>
-            <h3 className='info'>UTC -05:00</h3>
-          </div>
-          <button>|</button>
-          <div className='infoContainer'>
-            <h3 className='title'>ISP</h3>
-            <h3 className='info'>SpaceX Starlink</h3>
-          </div>
-        </div>
-      </div>
+      <MapContainer />
+      <AbsoluteContainer />
     </div>
   )
 }
---
+++ src/components/MapContainer.jsx
@@ -0,0 +1,7 @@
+import '../styles.css';
+
+export default function MapContainer() {
+  return (
+    <div className="mapContainer"></div>
+  )
+}
