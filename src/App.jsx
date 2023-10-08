a9dc2cf3376028b6767811c505f539739f4996b6
Nathan Lufuluabo <45732868+natelufuluabo@users.noreply.github.com>
2023-10-08 14:33:01 -0400

    fixed the bug with the css code

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
 import './styles.css';
+import { useState, useEffect } from 'react';
 import desktopBg from './images/pattern-bg-desktop.png';
+import mobileBg from './images/pattern-bg-mobile.png';
 
 function App() {
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+  useEffect(() => {
+    // Add an event listener to track screen width changes
+    const handleResize = () => {
+      setScreenWidth(window.innerWidth);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    // Remove the event listener when the component unmounts
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+  const imgSrc = screenWidth >= 426 ? desktopBg : mobileBg;
   return (
     <div className="mainContainer">
       <div className="bgImage">
-        <img src={desktopBg} />
+        <img src={imgSrc} />
       </div>
       <div className="mapContainer"></div>
       <div className="absoluteContainer">
