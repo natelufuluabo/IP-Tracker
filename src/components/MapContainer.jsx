cb4cae63c52d2a21a9fc0cbe87d1637ebb473e24
Nathan Lufuluabo <45732868+natelufuluabo@users.noreply.github.com>
2023-10-09 15:34:11 -0400

    added the ability to fly to a new loacation on the map based off of what the user has searched for

--- src/components/MapContainer.jsx
+++ src/components/MapContainer.jsx
@@ -1,6 +1,6 @@
 import '../styles.css';
 import 'leaflet/dist/leaflet.css';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { MapContainer, TileLayer, Marker } from 'react-leaflet';
 import { getUserLocation } from '../utils/utils'
 import { dataAtom } from '../utils/recoilStore';
@@ -9,19 +9,29 @@ import { useRecoilValue } from 'recoil';
 export default function InteractiveMapContainer() {
   const data = useRecoilValue(dataAtom);
   const [position, setPosition] = useState([34.05223, -118.24368]);
+  const mapRef = useRef(null);
   useEffect(() => {
     if (data.location.lat !== '' && data.location.lng !== '') {
+      const newCoordinates = [data.location.lat, data.location.lng];
       setPosition([data.location.lat, data.location.lng]); 
+      flyToCoordinates(newCoordinates);
       return 
     }
     getUserLocation().then((coordinates) => {
       const userCoordinates = [coordinates.latitude, coordinates.longitude];
       setPosition(userCoordinates);
+      flyToCoordinates(userCoordinates);
     })
-  }, [data.location.lat, data.location.lng])
+  }, [data.location.lat, data.location.lng]);
+
+  const flyToCoordinates = (coordinates) => {
+    if (mapRef.current) {
+      mapRef.current.flyTo(coordinates, 13, { duration: 1 }); 
+    }
+  };
   return (
     <>
-      <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
+      <MapContainer center={position} zoom={13} scrollWheelZoom={false} ref={mapRef}>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url='https://tile.openstreetmap.org/{z}/{x}/{y}.png'
